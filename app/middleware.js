e433a408649adfd17fb723437a421eaea288e7e6
hye <595507445@qq.com>
2026-04-22 21:59:23 +0800

    feat(view): add page rendering templates

--- app/middleware.js
+++ app/middleware.js
@@ -1,3 +1,14 @@
-module.exports = (app) => {
+const path = require('path');
 
+module.exports = (app) => {
+    const koaNunjucks2 = require('koa-nunjucks-2');
+
+    app.use(koaNunjucks2({
+        ext: 'tpl',
+        path: path.resolve(process.cwd(), './app/public'),
+        nunjucksConfig: {
+            noCache: true,
+            trimBlocks: true,
+        },
+    }));
 }
